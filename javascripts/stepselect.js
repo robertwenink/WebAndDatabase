fadc98e1d22ebc723aae71db87bc996069be0a1c
Robert Wenink <robertwenink@gmail.com>
2018-12-04 20:14:32 +0100

    Voortgang game + voorbeeld dam game

---
+++ javascripts/stepselect.js
@@ -0,0 +1,22 @@
+(function(){
+	// Get the container element
+	var btnContainer = document.getElementById("tiles");
+
+	// Get all buttons with class="btn" inside the container
+	var btns = btnContainer.getElementsByClassName("whitePiece");
+
+	// Loop through the buttons and add the active class to the current/clicked button
+	for (var i = 0; i < btns.length; i++) {
+	  btns[i].addEventListener("click", function() {
+		var current = document.getElementsByClassName("active");
+
+		// If there's no active class
+		if (current.length > 0) { 
+		  current[0].className = current[0].className.replace(" active", "");
+		}
+
+		// Add the active class to the current/clicked button
+		this.className += " active";
+	  });
+	}
+})();
