8421951bd037bc0ba6b32c5a91ad8c3818162206
Berend-Jan Lange <lange.berendjan@gmail.com>
2018-12-06 14:18:23 +0100

    update

--- checkers/app.js
+++ checkers/app.js
@@ -3,7 +3,7 @@ var http = require("http");
 var websocket = require("ws");
 
 var indexRouter = require("./routes/index");
-//var messages = require("./public/javascripts/messages");
+var messages = require("./public/javascripts/messages");
 
 var gameStatus = require("./statTracker");
 var Game = require("./game");
@@ -11,5 +11,17 @@ var Game = require("./game");
 var port = process.argv[2];
 var app = express();
 
+//app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);
+http.createServer(app).listen(port); //standerd file, not necessary in final version.
+/*app.get("/play", indexRouter);
+
+app.get("/", (req, res) => {
+  res.render("splash.ejs", { gamesInitialized: gameStatus.gamesInitialized, gamesCompleted: gameStatus.gamesCompleted, gamesAborted: gameStatus.gamesAborted });
+});
+
+var server = http.createServer(app);
+const wss = new websocket.Server({ server });
+
+var websockets = {};
+*/
